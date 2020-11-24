abe0393d40424a1cc85b4058e016291bf07fd2c7
santoshbk <bornfree.kushi@gmail.com>
2020-11-24 16:46:10 +0530

    Update A.java

--- LearningDSA/src/com/santosh/dsa/trees/A.java
+++ LearningDSA/src/com/santosh/dsa/trees/A.java
@@ -1,7 +1,30 @@
-package com.santosh.dsa.trees;
+package com.lc.challenges.trees;
 
-public class A {
-    public static void main(String[] args) {
-        System.out.println("cool");
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class PostOrderTravarsal {
+    public List<Integer> postorderTraversal(final TreeNode root) {
+        List<Integer> l = new ArrayList<>();
+        this.postOrder(root, l);
+        return l;
+    }
+
+    private void postOrder(final TreeNode root, final List<Integer> l) {
+        if (root == null) {
+            return;
+        }
+        this.postOrder(root.left, l);
+        this.postOrder(root.right, l);
+        l.add(root.val);
+    }
+
+    private void postOrderIterative(final TreeNode root, final List<Integer> l) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> s = new Stack<>();
+
     }
 }
