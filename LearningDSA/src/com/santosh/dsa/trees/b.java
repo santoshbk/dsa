e6437fb346b796a14b363488e505b4ccf9f95d04
santoshbk <bornfree.kushi@gmail.com>
2020-11-24 16:46:33 +0530

    Create b.java

---
+++ LearningDSA/src/com/santosh/dsa/trees/b.java
@@ -0,0 +1,41 @@
+package com.lc.challenges.trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class PreOrderTravarsal {
+    public List<Integer> preorderTraversal(final TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        this.preOrder(root, list);
+        return list;
+    }
+
+    public void preOrder(final TreeNode root, final List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        this.preOrder(root.left, list);
+        this.preOrder(root.right, list);
+    }
+
+    public void preOrderIterative(final TreeNode root, final List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> s = new Stack<>();
+        list.add(root.val);
+        s.push(root.right);
+        s.push(root.left);
+        while (!s.empty()) {
+            TreeNode x = s.pop();
+            if (x != null) {
+                list.add(x.val);
+                s.push(x.right);
+                s.push(x.left);
+            }
+        }
+    }
+}
