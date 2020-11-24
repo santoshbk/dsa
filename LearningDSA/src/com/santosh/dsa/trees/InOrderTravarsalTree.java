47e68105fdcd421c668c8b8959c0cdb8f3b9be5e
santoshbk <bornfree.kushi@gmail.com>
2020-11-24 16:44:58 +0530

    Create InOrderTravarsalTree.java

---
+++ LearningDSA/src/com/santosh/dsa/trees/InOrderTravarsalTree.java
@@ -0,0 +1,40 @@
+package com.lc.challenges.trees;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class InOrderTravarsalTree {
+    public List<Integer> inorderTraversal(final TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        this.inorder(root, list);
+        return list;
+    }
+
+    public void inorder(final TreeNode n, final List<Integer> list) {
+        if (n == null) {
+            return;
+        }
+        this.inorder(n.left, list);
+        list.add(n.val);
+
+        this.inorder(n.right, list);
+    }
+
+    public void inOrderIterative(final TreeNode n, final List<Integer> list) {
+        if (n == null) {
+            return;
+        }
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode curr = n;
+        while (curr != null || !st.empty()) {
+            while (curr != null) {
+                st.push(curr);
+                curr = curr.left;
+            }
+            curr = st.pop();
+            list.add(curr.val);
+            curr = curr.right;
+        }
+    }
+}
