e6572c9269e98f92f14f382a1327e693342f7299
santoshbk <bornfree.kushi@gmail.com>
2020-11-28 15:06:54 +0530

    Update InOrderTravarsalTree.java

--- LearningDSA/src/com/santosh/dsa/trees/InOrderTravarsalTree.java
+++ LearningDSA/src/com/santosh/dsa/trees/InOrderTravarsalTree.java
@@ -1,40 +1,51 @@
-package com.santosh.dsa.trees;
+package learning.dsa.trees;
 
-import java.util.ArrayList;
+import static org.junit.Assert.assertArrayEquals;
+
 import java.util.List;
-import java.util.Stack;
 
-public class InOrderTravarsalTree {
-    public List<Integer> inorderTraversal(final TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        this.inorder(root, list);
-        return list;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TreeDemoTest {
+
+    private BTreeNode root;
+    private TreeDemo t;
+
+    @Before
+    public void setUp() throws Exception {
+        this.t = new TreeDemo();
+        this.root = TreeDemo.getTempTree();
     }
 
-    public void inorder(final TreeNode n, final List<Integer> list) {
-        if (n == null) {
-            return;
-        }
-        this.inorder(n.left, list);
-        list.add(n.val);
-
-        this.inorder(n.right, list);
+    @Test
+    public void testinOder() {
+        List<Integer> inOder = this.t.inOrder(this.root);
+        List<Integer> inOderI = this.t.inOrderIterative(this.root);
+        assertArrayEquals(inOder.toArray(), inOderI.toArray());
     }
 
-    public void inOrderIterative(final TreeNode n, final List<Integer> list) {
-        if (n == null) {
-            return;
-        }
-        Stack<TreeNode> st = new Stack<>();
-        TreeNode curr = n;
-        while (curr != null || !st.empty()) {
-            while (curr != null) {
-                st.push(curr);
-                curr = curr.left;
-            }
-            curr = st.pop();
-            list.add(curr.val);
-            curr = curr.right;
-        }
+    @Test
+    public void testPreOrder() {
+        List<Integer> preOder = this.t.preOrder(this.root);
+        List<Integer> preOderI = this.t.preOrderIterative(this.root);
+        assertArrayEquals(preOder.toArray(), preOderI.toArray());
     }
+
+    @Test
+    public void testPostOrder() {
+        List<Integer> postOder = this.t.postOrder(this.root);
+        List<Integer> postOderI = this.t.postOrderIterative(this.root);
+//        assertArrayEquals(postOder.toArray(), postOderI.toArray());
+        Object[] expected = { 4, 5, 2, 6, 7, 3, 1 };
+        assertArrayEquals(expected, postOder.toArray());
+    }
+
+    @Test
+    public void testLevelOder() {
+        List<Integer> levelOder = this.t.levelOrder(this.root);
+        Object[] expected = { 1, 2, 3, 4, 5, 6, 7 };
+        assertArrayEquals(expected, levelOder.toArray());
+    }
+
 }
